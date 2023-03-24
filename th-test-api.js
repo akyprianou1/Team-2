44074c5135b71d82b6575c869811c02a8c099a26
SStyl <sstylianou1@uclan.ac.uk>
2023-03-24 21:02:23 +0200

    ....

--- th-test-api.js
+++ th-test-api.js
@@ -1,8 +1,43 @@
 function handleTestList(caller) {
-    handleList(caller, true);
+    //handleList(caller, true);
+    console.log(caller);
+    if ((Number(caller)<=Number(0))||(isNaN(caller)))
+    {
+        caller = 10;
+    }
+    let limit = "number-of-ths="+ caller;
+    let url = "https://codecyprus.org/th/test-api/list?" + limit;
+    console.log(url);
+    fetch(url)      // fetch data from the url
+        .then(response => response.json())
+        .then(jsonObject => {
+            console.log(jsonObject);
+
+            if (jsonObject.status === "OK") {
+                console.clear();
+                console.log(jsonObject.treasureHunts);
+                document.getElementById("treasureHunts").innerHTML = console.log(jsonObject.treasureHunts);
+            }
+            else
+            {
+                alert("Error");
+            }
+        });
 }
 
 function handleTestStart(caller) {
     let params = { "player": "INACTIVE" }; // explicitly request an error
     handleStart(params, caller, true);
 }
+
+
+function handleTestQuestion(caller){}
+
+
+function handleTestAnswer(caller){}
+
+
+function handleTestScore(caller){}
+
+
+function handleTestLeaderboard(caller){}
